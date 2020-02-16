a658a6ce947a4ab14a5b1a3e9cedbc7647e8e640
Mustafa1x <60532325+Mustafa1x@users.noreply.github.com>
2020-02-16 14:02:19 -0600

    Add files via upload
    Assignment 3 (index.js file)

---
+++ index.js
@@ -0,0 +1,54 @@
+const item = {
+    itemId: 'dtm2bd03-7bf4-49r6-8f0h-384f90c86eh66',
+    name: 'Drake Bulldog T-shirt',
+    description: 'Represent your school in this comfy T-shirt',
+    sizes: Small, Medium, Large, X-large,
+    price: 9.99,
+
+    itemId: 'jtg5cd03-3bf4-49f9-8f0b-380f90c46et6',
+    name: 'Drake Bulldogs Flannel',
+    description: 'The perfect casual top',
+    sizes: Small, Medium, Large, X-large,
+    price: 9.99,
+
+
+    itemId: 'def5cd03-7bf4-49e9-8f0b-380f90c86ec1',
+    name: 'Drake Bulldogs Sweatshirt',
+    description: 'Get comfortable in these sweats and look fly while wearing them',
+    sizes: Small, Medium, Large, X-large,
+    price: 19.99,
+
+	itemId: '504550bc-d21e-43c3-8767-0c1bd59a173c',
+    name: 'Drake Bulldogs Sweatpants',
+    description: 'Get comfortable in these sweats and look fly while wearing them',
+    sizes: 'Small', 'Medium', 'Large', 'X-large',
+    price: 19.99    
+};
+
+const customer = {
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'John.Doe@drake.edu',
+  customerId: 'randomid'
+};
+
+const cart = {
+  customerId: 'randomid',
+  createdDate: '2-16-2020',
+  purchasedDate: '2-16-2020'  
+};
+
+const cartItems = {
+  itemId: 'def5cd03-7bf4-49e9-8f0b-380f90c86ec1',
+  itemName: 'Drake Bulldogs Sweatshirt',
+  size: 'Medium'
+  quantity: '1'
+};
+
+console.log(item);
+
+console.log(customer);
+
+console.log(cart);
+
+console.log(cartItems);
