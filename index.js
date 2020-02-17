f776b5f6e7b660f980cd1c12a2dbba84bcb12bb4
Dheeraj Srivatsav <sdheeraj1992@gmail.com>
2020-02-16 20:26:31 -0600

    Update index.js
    cart items will be a list and each cart item will be part of a single cart so they will share the same cartId

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const item = {
-    itemId: 'dtm2bd03-7bf4-49r6-8f0h-384f90c86eh66',
+const cartItems = [
+	{itemId: 'dtm2bd03-7bf4-49r6-8f0h-384f90c86eh66',
     name: 'Drake Bulldog T-shirt',
     description: 'Represent your school in this comfy T-shirt',
-    sizes: Small, Medium, Large, X-large,
-    price: 9.99,
+    sizes: ['Small', 'Medium', 'Large', 'X-large'],
+    price: 9.99},
 
-    itemId: 'jtg5cd03-3bf4-49f9-8f0b-380f90c46et6',
+	{itemId: 'jtg5cd03-3bf4-49f9-8f0b-380f90c46et6',
     name: 'Drake Bulldogs Flannel',
     description: 'The perfect casual top',
-    sizes: Small, Medium, Large, X-large,
-    price: 9.99,
+    sizes: ['Small', 'Medium', 'Large', 'X-large']
+    price: 9.99},
 
 
-    itemId: 'def5cd03-7bf4-49e9-8f0b-380f90c86ec1',
+	{itemId: 'def5cd03-7bf4-49e9-8f0b-380f90c86ec1',
     name: 'Drake Bulldogs Sweatshirt',
     description: 'Get comfortable in these sweats and look fly while wearing them',
-    sizes: Small, Medium, Large, X-large,
-    price: 19.99,
+    sizes: ['Small', 'Large', 'X-large']
+    price: 19.99},
 
-	itemId: '504550bc-d21e-43c3-8767-0c1bd59a173c',
+	{itemId: '504550bc-d21e-43c3-8767-0c1bd59a173c',
     name: 'Drake Bulldogs Sweatpants',
     description: 'Get comfortable in these sweats and look fly while wearing them',
-    sizes: 'Small', 'Medium', 'Large', 'X-large',
-    price: 19.99    
-};
+    sizes: ['Small', 'Medium', 'Large']
+    price: 19.99}
+];
 
 const customer = {
   firstName: 'John',
@@ -38,12 +38,6 @@ const cart = {
   purchasedDate: '2-16-2020'  
 };
 
-const cartItems = {
-  itemId: 'def5cd03-7bf4-49e9-8f0b-380f90c86ec1',
-  itemName: 'Drake Bulldogs Sweatshirt',
-  size: 'Medium'
-  quantity: '1'
-};
 
 console.log(item);
 
